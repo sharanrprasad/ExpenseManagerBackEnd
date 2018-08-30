94870ff1c6c81e04e27f609e5bd2a66f2dd8366e
Sharan R Prasad <prasadshar1@ecs.vuw.ac.nz>
2018-08-30 13:27:56 +1200

    Changed defult adress

--- Program.cs
+++ Program.cs
@@ -41,6 +41,6 @@ namespace ExpenseManagerBackEnd
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>().UseUrls("https://127.0.0.1:5001").PreferHostingUrls(false);
+                .UseStartup<Startup>();
     }
 }
